a24fe853e2605461512dda6c46eb7d381f63872d
RoystonDAlmeida <roystonjan1@hotmail.com>
2024-11-23 19:38:58 +0530

    Configuration files

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// /** @type {import('next').NextConfig} */
+ 
+// const nextConfig = {
+//     experimental: {
+//       ppr: 'incremental',   // Enabling partial prerendering(PPR)
+//     },
+//   };
+   
+//   export default nextConfig;
